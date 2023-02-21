72a3e4814bea2b9ddc5739aaea10944e38784afc
congdanh2504 <danhuchiha2002@gmail.com>
2023-02-21 15:18:33 +0700

    Broadcast Receiver

---
+++ BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/AirplaneModeChangeReceiver.kt
@@ -0,0 +1,21 @@
+package com.example.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AirplaneModeChangeReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val isAirplaneModeEnabled = intent?.getBooleanExtra("state", false) ?: return
+
+        // checking whether airplane mode is enabled or not
+        if (isAirplaneModeEnabled) {
+            // showing the toast message if airplane mode is enabled
+            Toast.makeText(context, "Airplane Mode Enabled", Toast.LENGTH_LONG).show()
+        } else {
+            // showing the toast message if airplane mode is disabled
+            Toast.makeText(context, "Airplane Mode Disabled", Toast.LENGTH_LONG).show()
+        }
+    }
+}
---
+++ BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.broadcastreceiver
+
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var receiver: AirplaneModeChangeReceiver
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        receiver = AirplaneModeChangeReceiver()
+
+        // Intent Filter is useful to determine which apps wants to receive
+        // which intents,since here we want to respond to change of
+        // airplane mode
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            // registering the receiver
+            // it parameter which is passed in  registerReceiver() function
+            // is the intent filter that we have just created
+            registerReceiver(receiver, it)
+        }
+        val list = mutableListOf(1, 2, 3)
+        list.swap(0, 2)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
+    }
+
+    private fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
+        val tmp = this[index1] // 'this' corresponds to the list
+        this[index1] = this[index2]
+        this[index2] = tmp
+    }
+}
+
