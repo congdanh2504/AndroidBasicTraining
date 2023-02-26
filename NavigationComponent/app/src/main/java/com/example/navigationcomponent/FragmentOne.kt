5e9667f7efb20300e17798de10fd9a549c8082d4
congdanh2504 <danhuchiha2002@gmail.com>
2023-02-26 23:19:04 +0700

    Navigation component

---
+++ NavigationComponent/app/src/main/java/com/example/navigationcomponent/FragmentOne.kt
@@ -0,0 +1,23 @@
+package com.example.navigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import com.example.navigationcomponent.databinding.FragmentMainBinding
+
+
+class FragmentOne : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+}
---
+++ NavigationComponent/app/src/main/java/com/example/navigationcomponent/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.example.navigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.navigationcomponent.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btn.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_fragmentOne)
+        }
+    }
+
+}
