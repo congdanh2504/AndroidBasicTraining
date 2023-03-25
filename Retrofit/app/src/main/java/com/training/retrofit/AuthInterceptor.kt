e56b8d5f233a42d7c616e185dd34feb42e153fd0
congdanh2504 <danhuchiha2002@gmail.com>
2023-03-25 19:24:04 +0700

    Interceptor

---
+++ Retrofit/app/src/main/java/com/training/retrofit/AuthInterceptor.kt
@@ -0,0 +1,17 @@
+package com.training.retrofit
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(private val authToken: String) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+
+        val newRequest = originalRequest.newBuilder()
+            .header("Authorization", "Bearer $authToken")
+            .build()
+
+        return chain.proceed(newRequest)
+    }
+}
--- Retrofit/app/src/main/java/com/training/retrofit/RetrofitHelper.kt
+++ Retrofit/app/src/main/java/com/training/retrofit/RetrofitHelper.kt
@@ -1,5 +1,7 @@
 package com.training.retrofit
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -7,9 +9,15 @@ object RetrofitHelper {
 
     private const val baseUrl = "https://quotable.io/"
 
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(AuthInterceptor("my-auth-token"))
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .build()
+
     fun getInstance(): Retrofit {
         return Retrofit.Builder().baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
     }
 }
